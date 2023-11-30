f62c9bc3756b0c4120504e491f8b8b8b40e10efe
Maya Vasileva <maya.vassileva@gmail.com>
2023-11-30 18:33:59 +0000

    dec 1st

---
+++ src/main/java/Dec1.java
@@ -0,0 +1,5 @@
+public class Dec1 {
+    public void first(){
+        System.out.println("Code of Advent");
+    }
+}
---
+++ src/test/java/Dec1Test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class Dec1Test {
+
+    @Test
+    public void testFirst(){
+        Dec1 dec = new Dec1();
+        dec.first();
+    }
+}
